cb7eefb45c7f310373262ae3d61d24a78cb2744c
Kevin Burgulla Muriel <kevinburgulla@gmail.com>
2026-05-02 00:15:35 -0400

    feat: Add title verification to task_crud.ts

--- CRUD/task_crud.ts
+++ CRUD/task_crud.ts
@@ -14,3 +14,10 @@ class TaskService {
   return Math.random().tostring(36).substrinf(2,11);
  }
 
+ private validateTRitle(title: string): void {
+  if (!title || !title.trim()) {
+   throw new Error ("El título no puede estar vacío");
+  }
+ }
+
+

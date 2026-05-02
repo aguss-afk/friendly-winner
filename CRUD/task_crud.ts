fe1b52d623624c71bd83c63631394d75c3ad3b12
Kevin Burgulla Muriel <kevinburgulla@gmail.com>
2026-05-02 00:11:48 -0400

    feat: Add ID generation to task_crud.ts

--- CRUD/task_crud.ts
+++ CRUD/task_crud.ts
@@ -6,3 +6,11 @@ completed: boolkean;
 createdAt: Date;
 updatedAt: Date;
 };
+
+class TaskService {
+ private tasks: Task[] = [];
+
+ private generateId(): string{
+  return Math.random().tostring(36).substrinf(2,11);
+ }
+
